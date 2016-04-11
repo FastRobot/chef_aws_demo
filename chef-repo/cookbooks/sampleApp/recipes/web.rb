a323614be05ca0b3508ad574953104a7eb2e2ebb
Lamont Lucas <lamont@fastrobot.com>
2016-04-11 11:27:08 -0700

    adds the sinatra-contrib gem so I can use sinatra/config-file, reorderes the web resources for clarity

--- chef-repo/cookbooks/sampleApp/recipes/web.rb
+++ chef-repo/cookbooks/sampleApp/recipes/web.rb
@@ -6,18 +6,17 @@
 
 include_recipe 'build-essential'
 
-# the app sits in a directory under files/default/counterService
-# this is a terrible way to distribute software, but good enough for demo purposes
-remote_directory node['sampleApp']['appPath'] do
-  source 'counterService'
-end
-
-file "#{node['sampleApp']['appPath']}/config.yml" do
-  content "redis_host: localhost"
-end
-
-# from the poise cookbook example
+# from the poise cookbook application_ruby
 application node['sampleApp']['appPath'] do
+  # the app sits in a directory under files/default/counterService
+  # this is a terrible way to distribute software, just doing it for demo purposes
+  # but you could replace this with a git resource easily
+  remote_directory node['sampleApp']['appPath'] do
+    source 'counterService'
+  end
+  file "#{node['sampleApp']['appPath']}/config.yml" do
+    content "redis_host: localhost\n"
+  end
   bundle_install do
     deployment true
   end
