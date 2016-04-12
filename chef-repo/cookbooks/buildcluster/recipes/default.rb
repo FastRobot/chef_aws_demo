c8210f14a7a557e94e4eb39642dd36b56640fd3c
John Cook <john_cook@gap.com>
2016-04-11 21:54:45 -0700

    [JC] initial check-in of buildcluster

---
+++ chef-repo/cookbooks/buildcluster/recipes/default.rb
@@ -0,0 +1,101 @@
+#
+# Cookbook Name:: buildcluster
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+require 'chef/provisioning/aws_driver'
+with_driver "aws::#{node['buildcluster']['aws_region']}"
+
+with_chef_server node['buildcluster']['chef_server_url'],
+  :client_name => node['buildcluster']['chef_client_name'],
+  :signing_key_filename => node['buildcluster']['chef_signing_key_filename']
+
+aws_vpc 'aws-chef-vpc' do
+  cidr_block '10.0.0.0/23'
+  internet_gateway true
+  main_routes '0.0.0.0/0' => :internet_gateway
+  enable_dns_hostnames true
+end
+
+aws_subnet 'aws-chef-web-subnet' do
+  vpc 'aws-chef-vpc'
+  cidr_block '10.0.0.0/24'
+  map_public_ip_on_launch true
+end
+
+aws_subnet 'aws-chef-db-subnet' do
+  vpc 'aws-chef-vpc'
+  cidr_block '10.0.1.0/24'
+  map_public_ip_on_launch true
+end
+
+aws_security_group 'aws-chef-web-sg' do
+  vpc 'aws-chef-vpc'
+  inbound_rules '0.0.0.0/0' => [ 22, 80 ]
+end
+
+aws_security_group 'aws-chef-db-sg' do
+  vpc 'aws-chef-vpc'
+  inbound_rules '0.0.0.0/0' => [ 22 ],
+                'aws-chef-web-sg' => [ 6379 ]
+end
+
+#
+## Build the database box first
+machine 'db1' do
+  machine_options bootstrap_options: {
+    image_id: 'ami-df6a8b9b',
+    instance_type: 't2.micro',
+    subnet: 'aws-chef-db-subnet',
+    security_group_ids: ['aws-chef-db-sg']
+    },
+    convergence_options: {
+      ssl_verify_mode: :verify_none
+    }
+  recipe 'apt'
+  recipe 'sampleApp::db'
+end
+
+with_machine_options({
+  bootstrap_options: {
+    image_id: 'ami-06116566',
+    instance_type: 't2.micro',
+    subnet: 'aws-chef-web-subnet',
+    security_group_ids: ['aws-chef-web-sg']
+  },
+  convergence_options: {
+    ssl_verify_mode: :verify_none
+  }
+})
+
+#
+## Build the front-end webservers
+num_web_instances = 3
+1.upto(num_web_instances) do |inst|
+    machine "web#{inst}" do
+      recipe 'apt'
+      recipe 'sampleApp::web'
+    end
+end
+
+load_balancer "aws-chef-elb" do
+  machines (1..num_web_instances).map { |inst| "web#{inst}" }
+  load_balancer_options({
+    :listeners => [{
+      :port => 80,
+      :protocol => :http,
+      :instance_port => 9000,
+      :instance_protocol => :http,
+    }],
+    health_check: {
+      healthy_threshold:    2,
+      unhealthy_threshold:  4,
+      interval:             12,
+      timeout:              5,
+      target:               "HTTP:9000/"
+    },
+    subnets: "aws-chef-web-subnet",
+    security_groups: "aws-chef-web-sg"
+  })
+end
---
+++ chef-repo/cookbooks/buildcluster/recipes/teardown.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: buildcluster
+# Recipe:: teardown
+#
+# Copyright (c) 2016 Fast Robot, LLC, Apache 2.0
+
+# This recipe tears down the world
+require 'chef/provisioning/aws_driver'
+# Pick your aws region
+with_driver "aws::#{node['buildcluster']['aws_region']}"
+# Point to your Chef credentials...there is probably a better way to do this...
+with_chef_server node['buildcluster']['chef_server_url'],
+  :client_name => node['buildcluster']['chef_client_name'],
+  :signing_key_filename => node['buildcluster']['chef_signing_key_filename']
+
+# Delete the machines
+machine_batch do
+  action :destroy
+  machines ['web1', 'web2', 'web3']
+end
+
+load_balancer 'aws-chef-elb' do
+  action :destroy
+end
+
+# Purge the VPC to remove subnets and security groups
+aws_vpc 'aws-chef-vpc' do
+  action :purge
+end
+
+# Now delete the empty VPC
+aws_vpc 'aws-chef-vpc' do
+  action :destroy
+end
