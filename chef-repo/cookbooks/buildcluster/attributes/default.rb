fcf2139238bca59547d8b2f03235ab7702df8642
John Cook <john_cook@gap.com>
2016-04-15 19:43:27 -0700

    [JC] set chef_environment to _default and fixed the inspec tests in vagrant

--- chef-repo/cookbooks/buildcluster/attributes/default.rb
+++ chef-repo/cookbooks/buildcluster/attributes/default.rb
@@ -1,19 +1,18 @@
 #
 ## Edit this block on the top if you want a different AWS Region, or
 ## have manually installed the .chef directory outside /home/ubuntu/chef-repo/.chef
-# the AWS location (default is us-west-1)
-default['buildcluster']['aws_region'] = 'us-west-1'
 # PATH to your .chef directory from the chef-starter.zip
 default['buildcluster']['chef_dir'] = '/home/ubuntu/chef-repo/.chef'
-# The chef_environment to deploy our nodes into in
-default['buildcluster']['chef_environment'] = 'chef_aws_demo'
-
-#
+# the AWS location (default is us-west-1)
+default['buildcluster']['aws_region'] = 'us-west-1'
 ## machine details
 default['buildcluster']['image_id'] = 'ami-df6a8b9b'
 default['buildcluster']['instance_type'] = 't2.micro'
 # Version of chef_client to installed
 default['buildcluster']['chef_client_version'] = '12.8.1'
+# The chef_environment to deploy our nodes into in
+default['buildcluster']['chef_environment'] = '_default'
+
 #
 ## How many webservers do we want?
 default['buildcluster']['num_web_instances'] = 1
--- chef-repo/cookbooks/sampleApp/test/integration/default/inspec/default_spec.rb
+++ chef-repo/cookbooks/sampleApp/test/integration/default/inspec/default_spec.rb
@@ -6,10 +6,10 @@ end
 
 # unicorn directly
 describe command('wget -qO- http://localhost:9000') do
-  its('stdout') { should match (/This is request [0-9]/) }
+  its('stdout') { should match (/This page has been accessed [0-9] times./) }
 end
 
 # and nginx
 describe command('wget -qO- http://localhost:80') do
-  its('stdout') { should match (/This is request [0-9]/) }
+  its('stdout') { should match (/This page has been accessed [0-9] times./) }
 end
